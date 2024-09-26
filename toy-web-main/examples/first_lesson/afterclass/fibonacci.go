19726131c9df16108bdc71025de2046c41633ad6
vivi <vivi66557788@gmail.com>
2024-09-26 21:10:02 +0400

    fibona

--- toy-web-main/examples/first_lesson/afterclass/fibonacci.go
+++ toy-web-main/examples/first_lesson/afterclass/fibonacci.go
@@ -1,10 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("fibonacci 0-10 sum:")
+	for i := 1; i <= 10; i++ {
+		sum1 := fibonacci(i)
+		fmt.Println(sum1)
+	}
 }
 
 func fibonacci(n int) int {
 	// TODO
-	return 0
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
 }
